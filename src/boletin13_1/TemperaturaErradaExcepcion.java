be9e11e8a488ab3ad9a82478dbea1b315dc2e1d8
agonzalezgonzalez <agonzalezgonzalez@victoria4.danielcastelao.org>
2017-12-04 11:24:42 +0100

    Commit excepcion

---
+++ src/boletin13_1/TemperaturaErradaExcepcion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin13_1;
+
+/**
+ *
+ * @author agonzalezgonzalez
+ */
+public class TemperaturaErradaExcepcion extends Exception {
+
+    public TemperaturaErradaExcepcion() {
+        super("Error al introducir G.centigrados");
+    }
+     public TemperaturaErradaExcepcion(String mensaxe) {
+        super("Error al introducir G.centigrados");
+    }
+
+  
+}
